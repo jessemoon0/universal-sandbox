a7cf1e9a7435b325e55518b39a00058eaf17935c
Jessie J Valladares <shajessie@me.com>
2020-04-24 00:41:41 -0400

    Branch cleaning

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,4 @@ import * as functions from 'firebase-functions';
 
 const universal = require(`${process.cwd()}/dist/small-test/server/main`).app;
 
-export const ssr = functions.https.onRequest(universal());
+export const ssr = functions.https.onRequest(universal);
